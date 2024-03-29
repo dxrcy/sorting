a3ddff0ef6b256f7b08376f0b2abebeb2c36ec8e
darcy <acednes@gmail.com>
2024-03-29 15:31:25 +1100

    fix flickering - use crossterm

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
+use crossterm::{
+    cursor, execute,
+    style::{Color, ResetColor, SetBackgroundColor, SetForegroundColor},
+    terminal::{self, Clear, ClearType},
+};
 use rand::seq::SliceRandom;
 use std::io::{self, Write};
 
 use sorting::{colors::*, sorts::SelectionSort, Sorter, Value};
 
-fn main() {
-    const SIZE: usize = 40;
-    const FRAME_TIME: u64 = 20;
+fn main() -> io::Result<()> {
+    const SIZE: usize = 50;
 
     let mut list: Vec<_> = (1..=SIZE as Value).collect();
     let mut rng = rand::thread_rng();
@@ -13,40 +17,57 @@ fn main() {
 
     let iter = SelectionSort::new(&mut list);
 
+    terminal::enable_raw_mode()?;
+
+    execute!(
+        io::stdout(),
+        Clear(ClearType::All),
+        crossterm::cursor::Hide,
+        SetBackgroundColor(Color::Rgb { r: 0, g: 0, b: 0 }),
+    )?;
+
     for state in iter {
-        clear();
+        for (x, value) in state.list.iter().enumerate() {
+            let h = x as f64 * 360.0 / SIZE as f64;
+            let s = 100.0;
+            let l = if state.compare.is_some_and(|[a, b]| a == x || b == x) {
+                100.0
+            } else {
+                50.0
+            };
 
-        for y in 0..SIZE {
-            for (x, value) in state.list.iter().enumerate() {
-                let h = x as f64 * 360.0 / SIZE as f64;
-                let s = 100.0;
-                let mut l = 50.0;
+            let (r, g, b) = hsl_to_rgb(h, s, l);
 
-                if state.compare.is_some_and(|[a, b]| a == x || b == x) {
-                    l = 100.0;
-                    // l = (1.0 - ((y as f64 / SIZE as f64) * (*value as f64 / 10.0)).min(1.0)) * 50.0;
-                }
+            execute!(io::stdout(), SetForegroundColor(Color::Rgb { r, g, b }))?;
 
-                let (r, g, b) = hsl_to_rgb(h, s, l);
-                print!("\x1b[38;2;{};{};{}m", r, g, b);
+            for y in 0..SIZE {
+                execute!(
+                    io::stdout(),
+                    cursor::MoveTo(x as u16 * 2, SIZE as u16 - y as u16 - 1)
+                )?;
 
-                if *value as usize >= SIZE - y {
+                if y < *value as usize {
                     print!("{}", "\u{2588}".repeat(2));
                 } else {
                     print!("  ");
                 }
-
-                print!("\x1b[0m");
             }
-            println!();
         }
-
-        std::thread::sleep(std::time::Duration::from_millis(FRAME_TIME));
     }
+
+    execute!(
+        io::stdout(),
+        cursor::MoveTo(0, SIZE as u16),
+        cursor::Show,
+        ResetColor,
+    )?;
+    terminal::disable_raw_mode()?;
 
     if !is_sorted(&list) {
-        println!("{RED}{}{RESET}", "The list is not sorted");
+        println!("{BRIGHT}{RED}{}{RESET}", "The list is not sorted\r");
     }
+
+    Ok(())
 }
 
 fn clear() {
