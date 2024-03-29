6557736eb7fff470f2f2d2a51aa1281a25c2cd77
darcy <acednes@gmail.com>
2024-03-29 14:52:30 +1100

    better animation

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,40 @@ use std::io::{self, BufRead, Write};
 use sorting::{Compare, SortState, Sorter};
 
 fn main() {
-    let mut list = vec![2, 1, 4, 3];
+    let mut list = vec![7, 3, 6, 2, 9, 10, 1, 8, 5, 4];
 
     let iter = SelectionSort::new(&mut list);
 
-    for step in iter {
+    for state in iter {
         clear();
-        println!("{}", step);
-        // wait();
-        std::thread::sleep(std::time::Duration::from_millis(800));
-    }
 
-    println!("{:?}", list);
-    println!("sorted: {}", is_sorted(&list).to_string().to_uppercase());
+        for y in 0..10 {
+            for (x, value) in state.list.iter().enumerate() {
+                let h = x as f64 * 360.0 / 10.0;
+                let s = 100.0;
+                let mut l = 50.0;
+
+                if state.compare.is_some_and(|[a, b]| a == x || b == x) {
+                    l = (1.0 + ((y as f64 / 10.0) * (*value as f64 / 10.0)).min(1.0)) * 50.0;
+                }
+
+                let (r, g, b) = hsl_to_rgb(h, s, l);
+                print!("\x1b[38;2;{};{};{}m", r, g, b);
+
+                if *value >= 10 - y {
+                    print!("{}", "\u{2588}".repeat(2));
+                } else {
+                    print!("  ");
+                }
+
+                print!("\x1b[0m");
+            }
+            println!();
+        }
+
+        // wait();
+        std::thread::sleep(std::time::Duration::from_millis(100));
+    }
 }
 
 fn clear() {
@@ -24,6 +45,36 @@ fn clear() {
 }
 fn wait() {
     io::stdin().lock().read_line(&mut String::new()).unwrap();
+}
+
+fn hsl_to_rgb(mut h: f64, mut s: f64, mut l: f64) -> (u8, u8, u8) {
+    h /= 360.0;
+    s /= 100.0;
+    l /= 100.0;
+
+    let c = (1.0 - (2.0 * l - 1.0).abs()) * s;
+    let x = c * (1.0 - ((h * 6.0) % 2.0 - 1.0).abs());
+    let m = l - c / 2.0;
+
+    let (r, g, b) = if h < 1.0 / 6.0 {
+        (c, x, 0.0)
+    } else if h < 2.0 / 6.0 {
+        (x, c, 0.0)
+    } else if h < 3.0 / 6.0 {
+        (0.0, c, x)
+    } else if h < 4.0 / 6.0 {
+        (0.0, x, c)
+    } else if h < 5.0 / 6.0 {
+        (x, 0.0, c)
+    } else {
+        (c, 0.0, x)
+    };
+
+    let r = ((r + m) * 255.0).round() as u8;
+    let g = ((g + m) * 255.0).round() as u8;
+    let b = ((b + m) * 255.0).round() as u8;
+
+    (r, g, b)
 }
 
 fn is_sorted(list: &[i32]) -> bool {
