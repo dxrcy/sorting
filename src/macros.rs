63c42077379deb88411544c56bb0492d55ec18e8
darcy <acednes@gmail.com>
2024-04-06 19:12:01 +1100

    re-order macros

--- src/macros.rs
+++ src/macros.rs
@@ -1,46 +1,4 @@
-macro_rules! algorithm {
-    (
-        $name:ident : | $list:ident, $scope:ident |
-        { $($body:tt)* }
-    ) => {
-        #[allow(unused_imports)]
-        use crate::{Compare, ListRef, Value};
-
-        pub fn $name(mut $list: ListRef) -> generator::LocalGenerator<'static, (), Compare> {
-            generator::Gn::new_scoped_local(move |mut $scope| {
-                yield_!($scope, None);
-                {
-                    let $list = $list.as_mut_slice();
-                    $($body)*
-                }
-                yield_!($scope, None);
-                generator::done()
-            })
-        }
-    };
-}
-
-macro_rules! yield_ {
-    ($scope:expr, None $(,)?) => {
-        unsafe { $scope.yield_unsafe(None) };
-    };
-    ($scope:expr, [$($x:expr),*] $(,)?) => {
-        unsafe { $scope.yield_unsafe(Some( [$($x),*] )) };
-    };
-}
-
-// Wrapper for subslicing a `Slice` or `SliceMut` to preserve original whole slice
-macro_rules! slice {
-    ( & $list:ident, $start:expr, $end:expr ) => {{
-        let start = $list.start();
-        Slice::new($list.get_whole(), start + $start, start + $end)
-    }};
-    ( &mut $list:ident, $start:expr, $end:expr ) => {{
-        let start = $list.start();
-        SliceMut::new($list.get_whole_mut(), start + $start, start + $end)
-    }};
-}
-
+/// Declare generator function, and define test and identifier for each algorithm
 macro_rules! define_algorithms {
     ( $( $index:literal | $Name:ident => $name:ident ),* $(,)? ) => {
         /// Sorting algorithms
@@ -101,4 +59,47 @@ macro_rules! define_algorithms {
             }
         }
     };
+}
+
+macro_rules! algorithm {
+    (
+        $name:ident : | $list:ident, $scope:ident |
+        { $($body:tt)* }
+    ) => {
+        #[allow(unused_imports)]
+        use crate::{Compare, ListRef, Value};
+
+        pub fn $name(mut $list: ListRef) -> generator::LocalGenerator<'static, (), Compare> {
+            generator::Gn::new_scoped_local(move |mut $scope| {
+                yield_!($scope, None);
+                {
+                    let $list = $list.as_mut_slice();
+                    $($body)*
+                }
+                yield_!($scope, None);
+                generator::done()
+            })
+        }
+    };
+}
+
+macro_rules! yield_ {
+    ($scope:expr, None $(,)?) => {
+        unsafe { $scope.yield_unsafe(None) };
+    };
+    ($scope:expr, [$($x:expr),*] $(,)?) => {
+        unsafe { $scope.yield_unsafe(Some( [$($x),*] )) };
+    };
+}
+
+// Wrapper for subslicing a `Slice` or `SliceMut` to preserve original whole slice
+macro_rules! slice {
+    ( & $list:ident, $start:expr, $end:expr ) => {{
+        let start = $list.start();
+        Slice::new($list.get_whole(), start + $start, start + $end)
+    }};
+    ( &mut $list:ident, $start:expr, $end:expr ) => {{
+        let start = $list.start();
+        SliceMut::new($list.get_whole_mut(), start + $start, start + $end)
+    }};
 }
