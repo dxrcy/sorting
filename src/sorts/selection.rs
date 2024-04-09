37418bcc5e86b791f22d14e9bd3901ecec7973ef
darcy <acednes@gmail.com>
2024-04-10 09:47:34 +1000

    small selection sort bug

--- src/sorts/selection.rs
+++ src/sorts/selection.rs
@@ -8,7 +8,7 @@ algorithm!(selection: |list, scope| {
     for i in 0..list.len() - 1 {
         let mut min_index = i;
 
-        for j in i..list.len() {
+        for j in i + 1..list.len() {
             yield_!(scope, [j, min_index]);
 
             if list[j] < list[min_index] {
